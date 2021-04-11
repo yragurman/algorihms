7bd312314749231e52b2be29516135aa44c5269b
Yura <yura.gurman.01@gmail.com>
2021-04-12 01:47:17 +0300

    remove

--- lr1/Park.py
+++
@@ -1,5 +0,0 @@
-class Park:
-    def __init__(self,parkName,bicyclePathLength,entryPrice):
-        self.parkName = parkName
-        self.bicyclePathLength = bicyclePathLength
-        self.entryPrice = entryPrice
--- lr1/algorithmOperation.py
+++
@@ -1,8 +0,0 @@
-
-def swap(List,firstIndex,doubleIndex):
-    List[firstIndex], List[doubleIndex] = List[doubleIndex], List[firstIndex]
-    return List
-
-swapCountHeapSort = 0
-comparisonCountHeapSort = 0
-
